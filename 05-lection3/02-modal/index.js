2f4bdfed9a3de1363b8fc3fc6a7b8f1a8e0bef1a
loramisautova <misautova.lora@gmail.com>
2023-10-03 19:35:41 +0300

    add changes to modal component

--- 05-lection3/02-modal/index.js
+++ 05-lection3/02-modal/index.js
@@ -1,7 +1,6 @@
 (function() {
     const close = document.getElementById('modal-close');
     const modal = document.getElementById('modal');
-    const overlay = document.getElementById("modal-overlay");
     const modalOpen = document.getElementById('modal-open');
 
     if (!close || !modalOpen || !modal) {
@@ -10,13 +9,11 @@
 
     modalOpen.addEventListener('click', () => {
         modal.style.display = 'block';
-        overlay.style.display = 'block';
         document.body.overflow = 'hidden';
     });
 
     close.addEventListener('click', () => {
         modal.style.display = 'none';
-        overlay.style.display = 'none';
         document.body.overflow = 'initial';
     });
 })();
